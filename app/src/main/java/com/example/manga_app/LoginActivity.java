2150617fe38a765355e1ddbb2286bc0d148d9f38
RyzenInu <ricardojpj@hotmail.com>
2024-01-23 17:28:46 +0000

    started on LoginActivity

--- app/src/main/java/com/example/manga_app/LoginActivity.java
+++ app/src/main/java/com/example/manga_app/LoginActivity.java
@@ -1,8 +1,19 @@
 package com.example.manga_app;
 
+import static android.provider.ContactsContract.CommonDataKinds.Website.URL;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +21,17 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendLogin();
+            }
+        });
+    }
+
+    private void sendLogin() {
+
     }
 }

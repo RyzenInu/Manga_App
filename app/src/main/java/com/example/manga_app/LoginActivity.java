4ae028117ca4fc81cf3d5ef2f80bdcaf6f3b4120
RyzenInu <ricardojpj@hotmail.com>
2024-01-30 14:44:55 +0000

    Login and Register can now redirect to each other.

--- app/src/main/java/com/example/manga_app/LoginActivity.java
+++ app/src/main/java/com/example/manga_app/LoginActivity.java
@@ -1,22 +1,13 @@
 package com.example.manga_app;
 
-import static android.provider.ContactsContract.CommonDataKinds.Website.URL;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 public class LoginActivity extends AppCompatActivity {
 
--- app/src/main/java/com/example/manga_app/RegisterActivity.java
+++ app/src/main/java/com/example/manga_app/RegisterActivity.java
@@ -2,7 +2,12 @@ package com.example.manga_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +15,38 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        Button btnRegister = findViewById(R.id.btnRegister);
+        TextView btnLogin = findViewById(R.id.textViewSubtitleBtn);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(sendRegister()){
+                    Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                } else{
+                    Toast.makeText(RegisterActivity.this, "Couldn't Register", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private boolean sendRegister() {
+        // Fazer a request http ao servidor.
+        // Enviar no BODY da REQUEST, em formato json: { username: exemplo, firstname: exemplo, lastname: exemplo, email: exemplo, password: exemplo  }
+        // Esperar pela resposta (await/promise).
+        // O servidor responde com. { registered: false, error: erro } ou { registered: true }
+        // Se recebermos registered: false, mostramos um Toast (DONE)
+        // Se recebermos registered: true, redirecionamos para a login activity (DONE)
+        return true; // Temporário
     }
 }

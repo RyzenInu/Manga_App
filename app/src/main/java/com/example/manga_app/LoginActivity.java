8eb4ffcb6b81df3c0a2fdd42207118ef53f30322
RyzenInu <ricardojpj@hotmail.com>
2024-01-30 14:15:58 +0000

    Fragment + BottomNav navigation implemented.

--- app/src/main/java/com/example/manga_app/LoginActivity.java
+++ app/src/main/java/com/example/manga_app/LoginActivity.java
@@ -4,11 +4,14 @@ import static android.provider.ContactsContract.CommonDataKinds.Website.URL;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
@@ -23,15 +26,37 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         Button btnLogin = (Button) findViewById(R.id.btnLogin);
+        TextView btnRegister = (TextView) findViewById(R.id.textViewSubtitleBtn);
+        EditText editPassword = (EditText) findViewById(R.id.editTextTextPassword);
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                sendLogin();
+                if(sendLogin()){
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                } else{
+                    Toast.makeText(LoginActivity.this, "Couldn't Login", Toast.LENGTH_LONG).show();
+                    editPassword.setText("");
+                }
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
             }
         });
     }
 
-    private void sendLogin() {
-
+    private boolean sendLogin() {
+        // Fazer a request http ao servidor.
+        // Enviar no BODY da REQUEST, em formato json: { username: exemplo, password: exemplo }
+        // Esperar pela resposta (await/promise).
+        // O servidor responde com. { logged: false, error: erro } ou { logged: true }
+        // Se recebermos logged: false, mostramos um Toast e limpamos o campo da passe (já está feito)
+        // Se recebermos logged: true, redirecionamos para a main activity (já está feito)
+        return true; // Temporário
     }
 }
---
+++ app/src/main/java/com/example/manga_app/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.manga_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+import com.example.manga_app.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigation.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+            if (itemId == R.id.action_home) {
+                replaceFragment(new HomeFragment());
+            } else if (itemId == R.id.action_equipment) {
+                replaceFragment(new EquipmentFragment());
+            } else if (itemId == R.id.action_stats) {
+                replaceFragment(new StatsFragment());
+            }
+            return true;
+        });
+
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
+    }
+}
